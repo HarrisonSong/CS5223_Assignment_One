e88d539351e88c6dd09989fcfa07fa2beefa8255
Qiyue <harrisonsong1991@gmail.com>
2017-09-16 18:58:28 +0800

    add background ping implementation

---
+++ Game/src/Game/BackgroundPing/Pinger.java
@@ -0,0 +1,38 @@
+package Game.BackgroundPing;
+
+import Common.EndPoint;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class Pinger {
+    private static final int TIMEOUT = 500;
+    private InetSocketAddress destination;
+
+    public Pinger(EndPoint endPoint) {
+        this.destination = new InetSocketAddress(endPoint.getIPAddr(), endPoint.getPortNum());
+    }
+
+    public Pinger(String IP, int port) {
+        this.destination = new InetSocketAddress(IP, port);
+    }
+
+    public InetSocketAddress getDestination() {
+        return destination;
+    }
+
+    public boolean isReachable() {
+        Socket connection = new Socket();
+        try {
+            try {
+                connection.connect(this.destination, TIMEOUT);
+            } finally {
+                connection.close();
+            }
+            return true;
+        } catch (IOException ex) {
+            return false;
+        }
+    }
+}
