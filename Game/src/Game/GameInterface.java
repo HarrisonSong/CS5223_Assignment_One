0e21b93db67c8b0ceae1a9ec18fbfc4d88d21d6f
Qiyue <harrisonsong1991@gmail.com>
2017-09-22 16:36:04 +0800

    fix one typo

--- Game/src/Game/GameInterface.java
+++ Game/src/Game/GameInterface.java
@@ -5,8 +5,8 @@ import java.rmi.RemoteException;
 import java.util.List;
 
 public interface GameInterface extends Remote{
-    GameGlobalState executeRequest(String name, char request) throws RemoteException;
-    GameGlobalState join(EndPoint IPArdress, String name) throws RemoteException;
+    GameGlobalState executeRequest(String name, String request) throws RemoteException;
+    GameGlobalState join(EndPoint IPAddress, String name) throws RemoteException;
     boolean updateBackupGameState(GameGlobalState gameGlobalState) throws RemoteException;
     boolean promoteToBeBackup(GameGlobalState gameGlobalState) throws RemoteException;
     List<EndPoint> getPrimaryAndBackupEndPoints() throws RemoteException;
