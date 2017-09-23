a57c78780df7d2b18307715c0d778d40e411359a
Qiyue <harrisonsong1991@gmail.com>
2017-09-23 16:07:57 +0800

    add constructor for IdEndPointPair to restrict player name length

--- Game/src/Common/Pair/IdEndPointPair.java
+++ Game/src/Common/Pair/IdEndPointPair.java
@@ -1,7 +1,14 @@
 package Common.Pair;
 import Common.EndPoint;
+import Game.Game;
 
 public class IdEndPointPair extends Pair<String, EndPoint> {
+    public IdEndPointPair() {
+        super();
+        this.setA(new String(new char[Game.NAME_LENGTH]));
+        this.setB(new EndPoint());
+    }
+
     public IdEndPointPair(String name, EndPoint ed) {
         super(name, ed);
     }
