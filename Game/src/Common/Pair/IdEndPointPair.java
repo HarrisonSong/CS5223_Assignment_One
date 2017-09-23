bdaaa9f33963886efe1cac36f3f96132b0955c52
e0045139 <e0045139@u.nus.edu>
2017-09-23 15:51:57 +0800

    add IdEndPointPair

---
+++ Game/src/Common/Pair/IdEndPointPair.java
@@ -0,0 +1,24 @@
+package Common.Pair;
+import Common.EndPoint;
+
+public class IdEndPointPair extends Pair<String, EndPoint> {
+    public IdEndPointPair(String name, EndPoint ed) {
+        super(name, ed);
+    }
+
+    public String getId() {
+        return getA();
+    }
+
+    public void setId(String name) {
+        setA(name);
+    }
+
+    public EndPoint getEndPoint() {
+        return getB();
+    }
+
+    public void setEndPoint(EndPoint type) {
+        setB(type);
+    }
+}
